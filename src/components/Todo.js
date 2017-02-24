39fb7d8fb270c9ce4e23be157bd4e10e5ef5be33
Haider <headeralishah@gmail.com>
2017-02-24 12:00:28 +0500

    Delete done.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,22 +9,48 @@ class Todo extends Component {
     constructor(props) {
         super(props);
         let currentUser = firebase.auth().currentUser;
-        // console.log(props);
+        this.deleteTodoHandler = this.deleteTodoHandler.bind(this);
+        this.editTodoHandler = this.editTodoHandler.bind(this);
         const {
-            todos
+            todos,
+            editStatus
         } = this.props
         firebase.database().ref('todos/' + currentUser.uid).on('child_added', (data) => {
-            // console.log(data, '************');
             let obj = data.val();
             obj.id = data.key;
             let currentTodos = this.props.todos;
             currentTodos = currentTodos.concat(obj)
-            // this.setState({ todos: currentTodos })
             this.props.addTodos(currentTodos);
-            console.log(todos, 'obj');
+        })
+        firebase.database().ref('todos/' + currentUser.uid).on('child_changed', (data) => {
+            let obj = data.val();
+            obj.id = data.key;
+            let currentTodos = this.props.todos;
+            currentTodos = currentTodos.concat(obj)
+            this.props.addTodos(currentTodos);
         })
 
     }
+    editTodoHandler(ev) {
+        console.log(ev.target.dataset);
+    }
+    deleteTodoHandler(ev) {
+        let todoKey = ev.target.dataset.id;
+        let currentUser = firebase.auth().currentUser;
+        firebase.database().ref('todos/' + currentUser.uid + '/' + todoKey).remove()
+            .then((v) => {
+                let currentTodo = this.props.todos;
+                let indexRemove;
+                for (var i = 0; i < currentTodo.length; i++) {
+                    if (currentTodo[i].id === todoKey) {
+                        indexRemove = i;
+                    }
+                }
+                currentTodo = currentTodo.slice(0, indexRemove).concat(currentTodo.slice(indexRemove + 1));
+                this.props.addTodos(currentTodo);
+            });
+    }
+
     render() {
         console.log(this.props.todos, '-----------');
         return (
@@ -32,7 +58,12 @@ class Todo extends Component {
                 <br />
                 {this.props.todos.map((v, i) => {
                     return (
-                        <h1 key={i}>{v.todo} </h1>
+                        <h1 key={i} className='heading-2'>{v.todo} {(!this.props.editStatus) ? (
+                            <span>
+                                <button data-id={v.id} onClick={this.deleteTodoHandler} className="btn btn-primary">Delete</button>
+                                <button data-id={v.id} data-todo={v.todo} onClick={this.editTodoHandler} className="btn btn-primary">Edit</button>
+                            </span>
+                        ) : ''} </h1>
                     )
                 })
                 }
@@ -43,7 +74,8 @@ class Todo extends Component {
 }
 
 module.exports = connect(state => ({
-    todos: state.todo.todos
+    todos: state.todo.todos,
+    editStatus: state.todo.editStatus
 }), {
         addTodos
     })(Todo)
